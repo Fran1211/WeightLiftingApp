19749312ef822742c4e21bb1b5bf45130c4fe3df
Francisco Lovera <cisco.lovera08@gmail.com>
2025-02-11 12:03:34 -0500

    Updated Repository Class

--- src/main/java/com/fran/gymprog/lifts/LiftRepository.java
+++ src/main/java/com/fran/gymprog/lifts/LiftRepository.java
@@ -1,7 +1,23 @@
 package com.fran.gymprog.lifts;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
+
+@Repository
 public class LiftRepository {
 
-    private List <Lifts> lift = new ArrayList<>();
+    private List <Lifts> lifts = new ArrayList<>();
+
+    List<Lifts> findAll() {
+        return lifts;
+    }
+
+    @PostConstruct
+    private void init(){
+        lifts.add(new Lifts(ExerciseType.BICEP_CURL, 30, 8, "02/11"));
+        lifts.add(new Lifts(ExerciseType.OVERHEAD_PRESS, 25, 8, "02/11"));
+    }
+
 }
