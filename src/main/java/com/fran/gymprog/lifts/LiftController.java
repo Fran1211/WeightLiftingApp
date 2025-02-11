bbf12cf5732939be6e9f874c0a867833a8c57b43
Francisco Lovera <cisco.lovera08@gmail.com>
2025-02-11 12:16:37 -0500

    Added api/run

--- src/main/java/com/fran/gymprog/lifts/LiftController.java
+++ src/main/java/com/fran/gymprog/lifts/LiftController.java
@@ -4,13 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class LiftController {
 
-    @GetMapping("/hello")
-    String home() {
-           return "Hello Lifter";
+    private final LiftRepository liftRepository;
+
+    public LiftController(LiftRepository liftRepository){
+        this.liftRepository = liftRepository;
     }
 
+    @GetMapping("/api/runs")
+    List<Lifts> findAll(){
+        return liftRepository.findAll();
+    }
 
 }
